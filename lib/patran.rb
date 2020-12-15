81968f5fda674046dc02cb8a4166ea919ba11b00
Daiki Sato <git@doew.jp>
2020-12-16 02:42:10 +0900

    implement yamato uri

--- lib/patran.rb
+++ lib/patran.rb
@@ -2,5 +2,27 @@ require "patran/version"
 
 module Patran
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class TrackingNumber
+    def initialize(number:, courier:)
+      raise Patran::Error.new('Number and Courier is required.') if number.nil? || courier.nil?
+
+      @number = number
+      @courier = courier
+      @courier_class = Patran.CourierClass @courier
+    end
+
+    def official_tracking_uri
+      @courier_class.official_tracking_uri @number
+    end
+  end
+
+  def self.CourierClass(courier)
+    case courier
+    when 'yamato'
+      Patran::Yamato
+    else
+      raise Patran::Error.new('Not implemented courier.')
+    end
+  end
 end
---
+++ lib/patran/yamato.rb
@@ -0,0 +1,7 @@
+module Patran
+  class Yamato
+    def self.official_tracking_uri(number)
+      "http://jizen.kuronekoyamato.co.jp/jizen/servlet/crjz.b.NQ0010?id=#{number}"
+    end
+  end
+end
